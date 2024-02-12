11d85a65e3ba2abb61034715a45e37c2e960bd4e
Kamil <kamil@kodeo.no>
2024-02-12 17:04:04 +0100

    Failsafes for api results and debug mode handling

--- src/main.py
+++ src/main.py
@@ -12,6 +12,8 @@ import os
 import re
 import base64
 from flask import Flask
+import hashlib
+import sys
 
 extensionId = 'ilehaonighjijnmpnagapkhpcdbhclfg'
 CRX_URL = "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=98.0.4758.102&acceptformat=crx2,crx3&x=id%3D~~~~%26uc&nacl_arch=x86-64"
@@ -24,10 +26,23 @@ except:
     USER = ''
     PASSW = ''
 
+try:
+    ALLOW_DEBUG = os.environ['ALLOW_DEBUG']
+    if ALLOW_DEBUG == 'True':
+        ALLOW_DEBUG = True
+    else:
+        ALLOW_DEBUG = False
+except:
+    ALLOW_DEBUG = False
+
 # are they set?
 if USER == '' or PASSW == '':
     print('Please set GRASS_USER and GRASS_PASS env variables')
     exit()
+
+if ALLOW_DEBUG == True:
+    print('Debugging is enabled! This will generate a screenshot and console logs on error!')
+
 
 #https://gist.github.com/ckuethe/fb6d972ecfc590c2f9b8
 def download_extension(extension_id):
@@ -37,7 +52,30 @@ def download_extension(extension_id):
     with open("grass.crx", "wb") as fd:
         for chunk in r.iter_content(chunk_size=128):
             fd.write(chunk)
+    if ALLOW_DEBUG == True:
+        #generate md5 of file
+        md5 = hashlib.md5(open('grass.crx', 'rb').read()).hexdigest()
+        print('Extension MD5: ' + md5)
 
+
+
+def generate_error_report(driver):
+    if ALLOW_DEBUG == False:
+        return
+    #grab screenshot
+    driver.save_screenshot('error.png')
+    #grab console logs
+    logs = driver.get_log('browser')
+    with open('error.log', 'w') as f:
+        for log in logs:
+            f.write(str(log))
+            f.write('\n')
+
+    url = 'https://imagebin.ca/upload.php'
+    files = {'file': ('error.png', open('error.png', 'rb'), 'image/png')}
+    response = requests.post(url, files=files)
+    print(response.text)
+    print('Error report generated! Provide the above information to the developer for debugging purposes.')
 
 print('Downloading extension...')
 download_extension(extensionId)
@@ -66,7 +104,6 @@ except (WebDriverException, NoSuchDriverException) as e:
 
 #driver.get('chrome-extension://'+extensionId+'/index.html')
 print('Started! Logging in...')
-
 driver.get('https://app.getgrass.io/')
 
 sleep = 0
@@ -80,8 +117,9 @@ while True:
         time.sleep(1)
         print('Loading login form...')
         sleep += 1
-        if sleep > 30:
+        if sleep > 15:
             print('Could not load login form! Exiting...')
+            generate_error_report(driver)
             driver.quit()
             exit()
 
@@ -95,6 +133,9 @@ user.send_keys(USER)
 passw.send_keys(PASSW)
 submit.click()
 
+#id="chakra-toast-manager-top-right" is the toast
+
+
 sleep = 0
 while True:
     try:
@@ -106,6 +147,7 @@ while True:
         sleep += 1
         if sleep > 30:
             print('Could not login! Double Check your username and password! Exiting...')
+            generate_error_report(driver)
             driver.quit()
             exit()
 
@@ -114,7 +156,7 @@ driver.get('chrome-extension://'+extensionId+'/index.html')
 sleep = 0
 while True:
     try:
-        driver.find_element('xpath', '//*[contains(text(), "Network quality")]')
+        driver.find_element('xpath', '//*[contains(text(), "Open dashboard")]')
         break
     except:
         time.sleep(1)
@@ -122,20 +164,50 @@ while True:
         sleep += 1
         if sleep > 30:
             print('Could not load connection! Exiting...')
+            generate_error_report(driver)
             driver.quit()
             exit()
 
 print('Connected! Starting API...')
 #flask api
 app = Flask(__name__)
+
 @app.route('/')
 def get():
-    network_quality = driver.find_element('xpath', '//*[contains(text(), "Network quality")]').text
-    network_quality = re.findall(r'\d+', network_quality)[0]
-    lifetime_earnings = driver.find_element('xpath', '//*[contains(text(), "Lifetime earnings")]')
-    lifetime_earnings = lifetime_earnings.find_element('xpath','following-sibling::div')
-    lifetime_earnings = lifetime_earnings.find_element('xpath','child::p').text
-    return {'network_quality': network_quality, 'lifetime_earnings': lifetime_earnings}
+    try:
+        network_quality = driver.find_element('xpath', '//*[contains(text(), "Network quality")]').text
+        network_quality = re.findall(r'\d+', network_quality)[0]
+    except:
+        network_quality = False
+        print('Could not get network quality!')
+        generate_error_report(driver)
+
+    try:
+        token = driver.find_element('xpath', '//*[@alt="token"]')
+        token = token.find_element('xpath', 'following-sibling::div')
+        epoch_earnings = token.text
+    except Exception as e:
+        epoch_earnings = False
+        print('Could not get earnings!')
+        generate_error_report(driver)
+    
+    try:
+        #find all chakra-badge
+        badges = driver.find_elements('xpath', '//*[@class="chakra-badge"]')
+        #find the one with chakra-text that contains either "Connected" or "Disconnected"
+        connected = False
+        for badge in badges:
+            text = badge.find_element_by_xpath('child::div').text
+            if 'Connected' in text:
+                connected = True
+                break
+    except:
+        connected = False
+        print('Could not get connection status!')
+        generate_error_report(driver)
+
+    return {'connected': connected, 'network_quality': network_quality, 'epoch_earnings': epoch_earnings}
+
 
 app.run(host='0.0.0.0',port=80, debug=False)
 driver.quit()
